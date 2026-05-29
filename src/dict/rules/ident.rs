aa7268167c058722d4c34bd4a5457b55de1ceda7
daxida <galmaw@hotmail.com>
2026-05-29 17:47:45 +0200

    Keep only the more precise rule identifier

--- src/dict/rules/ident.rs
+++ src/dict/rules/ident.rs
@@ -5,6 +5,11 @@ pub fn rule_identifiers(source: Lang, short_tags: &[String]) -> String {
     tags_to_rules(source, short_tags).join(" ")
 }
 
+fn replace_rule<'a>(rules: &mut Vec<&'a str>, remove: &str, add: &'a str) {
+    rules.retain(|r| *r != remove);
+    rules.push(add);
+}
+
 fn tags_to_rules<'a>(source: Lang, short_tags: &'a [String]) -> Vec<&'a str> {
     let mut rules: Vec<_> = short_tags
         .iter()
@@ -15,20 +20,19 @@ fn tags_to_rules<'a>(source: Lang, short_tags: &'a [String]) -> Vec<&'a str> {
         Lang::Es => {
             if rules.contains(&"n") {
                 if short_tags.iter().any(|t| t == "sg") {
-                    rules.push("ns");
+                    replace_rule(&mut rules, "n", "ns");
                 }
                 if short_tags.iter().any(|t| t == "pl") {
-                    rules.push("np");
+                    replace_rule(&mut rules, "n", "np");
                 }
             }
         }
         Lang::Ja => {
             for tag in short_tags {
                 match tag.as_str() {
-                    "ichidan" => rules.push("v1"),
-                    "godan" => rules.push("v5"),
-                    // "adj" => rules.push("adj-i"), // we don't know
-                    "sa-row" => rules.push("vs"),
+                    "ichidan" => replace_rule(&mut rules, "v", "v1"),
+                    "godan" => replace_rule(&mut rules, "v", "v5"),
+                    "sa-row" => replace_rule(&mut rules, "v", "vs"),
                     _ => {}
                 }
             }
